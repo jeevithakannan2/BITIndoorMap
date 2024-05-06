5bed7728439bb1a00f3fd37a96a9f73780ce558c
Jeevitha Kannan K S <ksjeevithakannan123@gmail.com>
2024-05-06 09:20:33 +0530

    Project created

---
+++ backend/src/main/java/com/JS/BITIndoorMap/BitIndoorMapApplication.java
@@ -0,0 +1,13 @@
+package com.JS.BITIndoorMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitIndoorMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitIndoorMapApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/JS/BITIndoorMap/BitIndoorMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JS.BITIndoorMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitIndoorMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
