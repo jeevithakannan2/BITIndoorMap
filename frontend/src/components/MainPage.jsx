9ecfc7a26d328043e92b3f9a8007f6c53c64f457
JEEVITHA KANNAN K S <ksjeevithakannan123@gmail.com>
2024-08-10 23:36:59 +0530

    Added bounds for the map

--- frontend/src/components/MainPage.jsx
+++ frontend/src/components/MainPage.jsx
@@ -4,23 +4,32 @@ import L from "leaflet";
 
 const LeafletMap = () => {
   useEffect(() => {
+    // Define the bounding box coordinates
+    const southWest = L.latLng(11.49128588728251, 77.27326626619613);
+    const northEast = L.latLng(11.50151943736928, 77.2823433200042);
+    const bounds = L.latLngBounds(southWest, northEast);
+
     const map = L.map("map", {
       attributionControl: false,
       zoomSnap: 1,
       zoomDelta: 1,
       wheelDebounceTime: 100,
-    }).setView([11.4956, 77.2777], 16);
+      maxBounds: bounds,
+      maxBoundsViscosity: 1.0
+    }).fitBounds(bounds);
 
     const tilesource_layer = L.tileLayer(
       "http://localhost:5173/map/{z}/{x}/{y}.png",
       {
         minZoom: 17,
         maxZoom: 19,
+        bounds: bounds
       },
     ).addTo(map);
 
     // Custom zoom handler
     const customZoomHandler = (e) => {
+      e.preventDefault();
       const currentZoom = map.getZoom();
       const newZoom = e.deltaY < 0 ? currentZoom + 1 : currentZoom - 1;
       map.setZoom(newZoom);
@@ -34,9 +43,10 @@ const LeafletMap = () => {
       map.remove();
     };
   }, []);
+
   return (
     <div id="map" style={{ width: "100%", height: "100vh" }}>
-      { }
+      {}
     </div>
   );
 };
